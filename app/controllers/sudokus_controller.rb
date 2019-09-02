75c59d7dd23a487a13badf37d7977c783053a931
sandra <sandraescobedobadia@gmail.com>
2019-09-02 06:27:50 +0200

    feat: clean controller functions

--- app/controllers/sudokus_controller.rb
+++ app/controllers/sudokus_controller.rb
@@ -1,11 +1,5 @@
 class SudokusController < ApplicationController
-  before_action :set_sudoku, only: [:show, :edit, :update, :destroy]
-
-  # GET /sudokus
-  # GET /sudokus.json
-  def index
-    @sudokus = Sudoku.all
-  end
+  before_action :set_sudoku, only: [:show, :edit, :destroy]
 
   # GET /sudokus/1
   # GET /sudokus/1.json
@@ -17,15 +11,10 @@ class SudokusController < ApplicationController
     @sudoku = Sudoku.new
   end
 
-  # GET /sudokus/1/edit
-  def edit
-  end
-
   # POST /sudokus
   # POST /sudokus.json
   def create
     @sudoku = Sudoku.new(sudoku_params)
-
     respond_to do |format|
       if @sudoku.save
         format.html { redirect_to @sudoku, notice: 'Sudoku was successfully created.' }
@@ -37,26 +26,12 @@ class SudokusController < ApplicationController
     end
   end
 
-  # PATCH/PUT /sudokus/1
-  # PATCH/PUT /sudokus/1.json
-  def update
-    respond_to do |format|
-      if @sudoku.update(sudoku_params)
-        format.html { redirect_to @sudoku, notice: 'Sudoku was successfully updated.' }
-        format.json { render :show, status: :ok, location: @sudoku }
-      else
-        format.html { render :edit }
-        format.json { render json: @sudoku.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /sudokus/1
   # DELETE /sudokus/1.json
   def destroy
     @sudoku.destroy
     respond_to do |format|
-      format.html { redirect_to sudokus_url, notice: 'Sudoku was successfully destroyed.' }
+      format.html { redirect_to new_sudoku_path, notice: 'Sudoku was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
