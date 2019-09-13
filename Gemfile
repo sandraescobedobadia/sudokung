253a0266093818347e4320b5d103d3b259a9c170
sandra <sandraescobedobadia@gmail.com>
2019-09-13 06:28:27 +0200

    test: adjust controller tests

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem 'webpacker', '~> 4.0'
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'factory_bot_rails'
-  gem 'rspec-rails', '~> 3.8'
+  gem 'rspec-rails', git: 'https://github.com/rspec/rspec-rails', branch: '4-0-dev'
 end
 
 group :development do
--- spec/controllers/sudoku_controller_spec.rb
+++ spec/controllers/sudoku_controller_spec.rb
@@ -2,17 +2,59 @@ require 'rails_helper'
 
 RSpec.describe SudokusController, type: :controller do
   describe 'GET #new' do
-    it 'assigns @sudoku' do
-      # get :new
-      # expect(assigns(:sudoku)).to be_a_new(Sudoku)
+    before do
+      get :new
     end
 
     it 'returns a success response' do
       expect(response).to have_http_status(200)
     end
+
+    it 'assigns @sudoku' do
+      expect(assigns(:sudoku)).to be_a_new(Sudoku)
+    end
+
+    it 'assigns @values to Array' do
+      expect(assigns(:values)).to be_kind_of(Array)
+    end
+
+    it '@values size is NROWS' do
+      expect(assigns(:values).size).to be_equal(Sudoku::NROWS)
+    end
+
+    it '@rows are differents' do
+      expect(assigns(:values).uniq).to match_array(assigns(:values))
+    end
+
+    it '@values are differents' do
+      differents = true
+      (0..8).each do |index|
+        differents = false if assigns(:values)[index].uniq != assigns(:values)[index]
+      end
+      expect(differents).to be true
+    end
+
   end
 
   describe 'POST #create' do
+    before do
+      @params =
+        { 'sudoku' =>
+          { "rows_attributes" =>
+            { "0" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "1" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "2" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "3" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "4" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "5" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "6" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "7" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "8" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}}
+            }
+          }
+        }
+    end
+
     it 'assigns @sudoku' do
       expect(assigns(:sudoku)).to eq(Sudoku.last)
     end
@@ -35,6 +77,21 @@ RSpec.describe SudokusController, type: :controller do
 
   describe 'DELETE #destroy' do
     before do
+      @params =
+        { 'sudoku' =>
+          { "rows_attributes" =>
+            { "0" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "1" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "2" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "3" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "4" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "5" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "6" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "7" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}},
+              "8" => {"values_attributes" => {"0" => {"number" => "1"}, "1" => {"number" => "2"}, "2" => {"number" => "3"}, "3" => {"number" => "4"}, "4" => {"number" => "5"}, "5" => {"number" => "6"}, "6" => {"number" => "7"}, "7" => {"number" => "8"}, "8" => {"number" => "9"}}}
+            }
+          }
+        }
       post :create, params: @params
       @sudoku_id = Sudoku.last.id
       post :create, params: @params
